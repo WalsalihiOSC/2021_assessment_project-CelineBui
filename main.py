999ce60f62b166ead8d158f8619c928dece65f9b
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-07-21 21:17:39 +1200

    Update main.py

--- main.py
+++ main.py
@@ -68,11 +68,37 @@ class exercise_win(Frame):
 
 '''Frame 3 - Scoreboard page'''
 class scoreboard_win(Frame):
-    pass
+    class scoreboard_win(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+
+        #Interface design
+        self.frame = LabelFrame(master, background="orange")
+        self.frame.grid(column=0, row=0)
+
+        self.label = Label(self.frame, text="Your score is: ")
+        self.label.grid(row=0)
+        self.box = LabelFrame(self.frame, width=50)
+        self.box.grid(row=1)
+        self.score_display = Label(self.frame, text="score variable")
+        
+        self.b1 = Button(self.frame, text="Retry", command=self.retry)
+        self.b1.grid(row=3, column=1)
+        self.b2 = Button(self.frame, text="New Player", command=self.new_player)
+        self.b2.grid(row=3, column=2)
+
+    def calculate_score():
+        pass
+
+    def retry():
+        pass
+    
+    def new_player():
+        pass
 
 
 root = Tk()
-exercise_win(root)
+scoreboard_win(root)
 root.title("Ormiston Computing")
 root.mainloop()
 
