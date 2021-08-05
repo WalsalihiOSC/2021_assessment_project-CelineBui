4e587858c8c2493fdf1a77bbf7a33d459699da04
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-05 22:24:08 +1200

    Complete frame's basic layout

--- welcome_win.py
+++ welcome_win.py
@@ -2,71 +2,64 @@ from tkinter import *
 from student import Student
 
 class WelcomeWindow(Frame):
-    def __init__(self, container):
-        Frame.__init__(self, container, bg="yellow")
+    def __init__(self, master=None):
+        Frame.__init__(self)
         self.student = []
         #Interface - blank fields and buttons
+
         self.sn = StringVar() #Variable for name
-        self.name_input = Entry(self, textvariable=self.sn)
-        self.name_input.grid(column=0, row=0)
+        self.name_input = Entry(self, textvariable=self.sn, borderwidth=3)
+        self.name_input.grid(column=0, row=0, padx=10, pady=10)
 
-        self.conf = Button(self, text="That's my name!")
-        self.conf.grid(column=0, row=1)
-        self.conf.configure(command=self.check_name)
+        self.yr = IntVar()
+        self.year_level = Entry(self, textvariable=self.yr, borderwidth=3, width=10)
+        self.year_level.grid(column=1, row=0, padx=10, pady=10)
 
-        self.difficulty = IntVar()
-        self.b1 = Button(self, 
-                        text="Easy",
-                        command=self.easy
-                        ).grid(column=2, row=0, padx=50, pady=20)
-        self.b2 = Button(self, 
-                        text="Kinda easy",
-                        command=self.med
-                        ).grid(column=2, row=1)
-        self.b3 = Button(self, 
-                        text="Not so easy",
-                        command=self.hard
-                        ).grid(column=2, row=2)
+        self.conf = Button(self, text="LET'S START!")
+        self.conf.grid(column=2, row=4, pady=15, ipady=10, ipadx=10, sticky=E)
+        self.conf.configure(command=self.check_info)
+
+        self.diff = IntVar()
+        self.diff.set(1) #intialise choices i.e easy
+
+        choices = [ ("Easy", 101),
+                    ("Kinda easy", 102),
+                    ("Not so easy", 103)]
+
+        self.sideframe = LabelFrame(self, height=10).grid(column=2, row=0)
+        for choice, val in choices:
+            Radiobutton(self.sideframe,
+                        text = choice,
+                        indicatoron = 0,
+                        width=10,
+                        height=3,
+                        variable = self.diff,
+                        command = self.confirm,
+                        value = val).grid(column=2, sticky=W, padx=50, pady=(10,0))
 
         self.grid()
 
-    def easy(self):
-        self.diff = "Easy"
-        self.store_info()
-    def med(self):
-        self.diff = "Medium"
-        self.store_info()
-    def hard(self):
-        self.diff = "Hard"
-        self.store_info()
-
-    def check_name(self):
+    def check_info(self):
         if self.sn == " ":
             Label(self, text="Fill in your name!", fg="red").grid(column=0, row=2)
         
     def store_info(self):
         #storing info when user clicks any of the difficulty level buttons to move forward
         self.student.append(self.name_input.get(), self.diff())
-        self.check_diff()
+        self.confirm()
 
-    def check_diff(self):
-        if len(self.student) == 3:
-            self.student[2] = None
-        Label(self, text=f"You've chosen {self.diff} mode!")
+    def confirm(self):
+        '''if len(self.student) == 3:
+            self.student[2] = None'''
+        Label(self.sideframe, text=f"You've chosen {self.diff} mode!")
+
 
     def reset(self):
         self.name_input.delete(0, 'end')
         self.difficulty = None
 
-class App(Tk):
-    def __init__(self):
-        super().__init__()
-
-        self.title('Ormiston Computing')
-        self.geometry('800x320')
-        self.resizable(False, False)
-
-if __name__ == "__main__":
-    root = App()
-    WelcomeWindow(root)
-    root.mainloop()
+root = Tk()
+root.title("Ormiston Computing")
+root.geometry("800x320")
+WelcomeWindow(root)
+root.mainloop()
