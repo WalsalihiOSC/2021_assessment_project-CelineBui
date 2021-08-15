b6b57384f624964137fa401ddac9c582e4f55e75
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-16 10:05:49 +1200

    Generating question - Working on answer checking

--- exercise_win.py
+++ exercise_win.py
@@ -21,33 +21,58 @@ class QuestionGenerator:
             QuestionGenerator.medium()
         return f' ' 
 
+class SideBar(Frame):
+    def __init__(self, a, master=None):
+        Frame.__init__(self, background="grey")
+        
+        self.box = LabelFrame(self, background="orange", width=240, height=350)
+        self.box.grid()
+
+        Label(self, text=a).grid(column=0, row=0, padx=45, pady=0)
+        self.grid(row=0, column=3, rowspan=3, sticky=E) 
+
 '''Frame 2 - Exercise page'''
 class ExerciseWindow(Frame):
-    def __init__(self, container):
-        super().__init__(container, background="grey")
+    def __init__(self, master=None):
+        Frame.__init__(self, background="blue")
         #White container
         self.white = LabelFrame(self, 
-                                width=530, 
-                                height=290,
-                                background='#D0F6FC')
+                                width=630, 
+                                height=300,
+                                background='#D0F6FC') #light blue
         self.white.grid(        column=0, 
                                 row=0, 
-                                padx=50, 
-                                pady=69)
-        self.q = Label(self.white,
-                        text="Auto-generated question")
-        self.q.grid(row=0, column=0, columnspan=2, padx=100, pady=20)
+                                padx=30, 
+                                pady=(33, 100))
+        self.submit = Button(self.white,
+                             text="Submit", command=self.check_answer)
+        self.submit.grid(row=3, column=0, columnspan=2, padx=180, pady=(20,20))
         self.label = Label(self.white, text="Answer: ")
         self.label.grid(row=2, column=0, padx=(100,0))
 
         self.a = IntVar()
         self.answer = Entry(self.white, textvariable=self.a)
-        self.answer.grid(row=2, column=1, padx=(0, 100), pady=50)
-
+        self.answer.grid(row=2, column=1, padx=(0, 100))
+        self.question_generator()
         self.grid()
 
+    def question_generator(self):
+        i = 0
+        while i < 21:
+            self.q_num = Label(border=1, background="yellow", text=i)
+            self.r = random.sample(range(0,100), 3)
+            a = f'{self.r[0]} + {self.r[1]} - {self.r[2]} = ?'
+            self.check_answer()
+            i+=1
+        self.q = Label(self.white, background="#D0F6FC", text=a)
+        self.q.grid(row=0, column=0, columnspan=2, padx=200, pady=50)  
+    
     def check_answer(self):
-        self.ans = int(self.answer.get())
+        '''self.ans = int(self.answer.get())
+        self.question = self.r[0] + self.r[1] - self.r[2]
+        if self.ans == self.question:
+            SideBar('Correct!')
+        else: SideBar('Incorrect!')'''
         #Change input field into int
         pass
 
@@ -55,15 +80,6 @@ class ExerciseWindow(Frame):
         #For addition and subtraction 
         #For multiplication and division
         pass
-
-class SideBar(LabelFrame):
-    def __init__(self, container):
-        super().__init__(container, background='blue')
-
-        self.grid(row=0, column=3, sticky=E)
-
-        self.nudge = Label(self, text="Star is here to encourage you!")
-        self.nudge.grid(column=0, row=0, padx=25, pady=152)
 
 class ControlFrame:
     def __int__ (self):
