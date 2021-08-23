baf0f3c16f53abb7b471dde4fd98639b910cd994
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-23 20:21:27 +1200

    Switch Frame function - First attempt

--- main.py
+++ main.py
@@ -1,199 +1,158 @@
 #Math programme for Ormiston primary students
 #By Celine Bui - Start: 12.07.21
 from tkinter import *
+from tkinter import ttk
 from student import Student
+from welcome_win import WelcomeWindow
+from exercise_win import ExerciseWindow, SideBar
+from scoreboard_win import ScoreboardWindow
+import random
 
-'''this should go in exercise_win.py'''
-class QuestionGenerator: 
-    @staticmethod
-    def easy(a, format=True):
-        r = random.sample(range(0,100), 3)
-        if format:
-            return f'{r[1]} + {r[2]} - {r[3]} = ?'
-        if r[1] + r[2] - r[3] == a:
-            QuestionGenerator.easy()
-        return f' '
 
-    @staticmethod
-    def medium(a, format=True):
-        r = random.sample(range(0,1000), 3)
-        if format:
-            return f'{r[1]} x {r[2]} - {r[3]} = ?'
-        if r[1] * r[2] - r[3] == a:
-            QuestionGenerator.easy()
-        return f' '
+'''Functions for switching between frames - Ref: https://www.pythontutorial.net/tkinter/tkraise/'''
+class SwitchFrame(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(WelcomeWindow)
 
-'''Frame 1 - Welcome page'''
-class welcome_win(Frame):
-    def __init__(self, master=None):
+        self.title("Ormiston Computing")
+        self.geometry('1000x500')
+        self.resizable(False, False)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.grid()  
+    
+class WelcomeWindow(Frame):
+    def __init__(self, master):
         Frame.__init__(self, master)
-
-        #Variables
-        self.sn = StringVar()
-        
-        #Frame
-        self.frame = LabelFrame(master)
-        self.frame.grid()
-        '''this frame might be unnecessary - fix later'''
-
         #Interface - blank fields and buttons
-        self.name_input = Entry(self.frame, textvariable=self.sn)
-        self.name_input.grid(column=0, row=0)
-        
-        Radiobutton(self.frame, 
-                    text="Easy", 
-                    value=0, #not to be confused with switching frame values for the whole programme
-                    variable=self.difficulty, #self.difficulty goes as if conditions into exercise_win
-                    command=self.store_info).grid(column=2, row=0)
-        Radiobutton(self.frame, 
-                    text="Kinda easy", 
-                    value=1, 
-                    variable=self.difficulty, 
-                    command=self.store_info).grid(column=2, row=1)
-        Radiobutton(self.frame, 
-                    text="Not so easy", 
-                    value=2, 
-                    variable=self.difficulty, 
-                    command=self.store_info).grid(column=2, row=2)
+
+        self.sn = StringVar() #Variable for name
+        self.name_input = Entry(self, textvariable=self.sn, borderwidth=3)
+        self.name_input.grid(column=0, row=2, padx=10, pady=10)
+
+        self.yr = IntVar()
+        self.year_level = Entry(self, textvariable=self.yr, borderwidth=3, width=10)
+        self.year_level.grid(column=1, row=2, padx=10, pady=10)
+
+        Button(self, text="NEXT", command=lambda: master.switch_frame(ExerciseWindow)).grid()
+
+        self.diff_value = IntVar()
+        self.diff_value.set(1) #intialise choices i.e easy
+
+        choices = [ ("Easy", 101),
+                    ("Kinda easy", 102),
+                    ("Not so easy", 103)]
+        #buttons for difficulty levels
+        #Code ref: https://www.python-course.eu/tkinter_radiobuttons.php 
+        for choice, val in choices:
+            self.choices = Radiobutton(self,
+                                       text = choice,
+                        indicatoron = 0,
+                        width=10,
+                        height=3,
+                        variable = self.diff_value,
+                        command = self.confirm,
+                        value = val).grid(column=2, sticky=W, padx=50, pady=(10,0))
+        self.grid()
 
     def check_info(self):
-        pass
-    
+        self.input_check = 1
+        if len(self.name_input.get()) == 0:
+            Label(self, text="Fill in your name!", fg="red").grid(column=0, row=1)
+            self.input_check = 0
+        self.confirm()
+        
     def store_info(self):
         #storing info when user clicks any of the difficulty level buttons to move forward
-        self.student = Student(self.name_input.get())
-        pass
-    
-'''This class goes with the static methods - probably also into exercise_win'''
-'''welcome_win will have own version of ControlFrame for moving to the right exercise frame? + storing info'''
-class ControlFrame1(LabelFrame):
-    def __init__(self, container):
-        super().__init__(container)
-        self['text'] = 'Difficulty levels'
+        self.student.append(self.name_input.get(), self.diff())
 
-        self.difficulty = IntVar()
-        Radiobutton(self, 
-                    text="Easy", 
-                    value=0, #not to be confused with switching frame values for the whole programme
-                    variable=self.difficulty, #self.difficulty goes as if conditions into exercise_win
-                    command=self.change_frame).grid(column=2, row=0)
-        Radiobutton(self, 
-                    text="Kinda easy", 
-                    value=1, 
-                    variable=self.difficulty, 
-                    command=self.change_frame).grid(column=2, row=1)
-        Radiobutton(self, 
-                    text="Not so easy", 
-                    value=2, 
-                    variable=self.difficulty, 
-                    command=self.change_frame).grid(column=2, row=2)
+    def confirm(self):
+        '''if len(self.student) == 3:
+            self.student[2] = None'''
+        if self.diff_value == 101 or 102 or 103:
+            self.diff = self.choices.text
+            return self.diff
+        Label(self, text=f"You've chosen {self.diff} mode!").grid(column=2, row=5)
+        self.store_info()
 
-        self.grid(column=1, row=0, sticky='ns')
-        #Intialise exercise frames transition
-        self.frames = {}
+    def reset(self):
+        self.name_input.delete(0, 'end')
+        self.difficulty = None
 
-        self.change_frame()
-
-    def change_frame(self):
-        self.student.append()
-        frame = self.frames[self.difficulty.get()]
-        frame.reset()
-        frame.tkraise()
-        
-        
-        
-
-'''Frame 2 - Exercise page'''
-class exercise_win(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-
-        #Interface design
-        self.frame = LabelFrame(master, highlightbackground="yellow")
-        self.frame.grid(column=0, row=0)
-
+class ExerciseWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, background="blue")
         #White container
-        self.white = LabelFrame(self.frame, width=130, height=200)
-        self.white.grid(column=0, row=0)
-        self.q = Label(self.white,
-                        text="Auto-generated question")
-        self.q.grid(row=0)
+        self.i = 0
+        self.white = LabelFrame(self, 
+                                width=630, 
+                                height=300,
+                                background='#D0F6FC') #light blue
+        self.white.grid(        column=0, 
+                                row=0, 
+                                padx=30, 
+                                pady=(33, 100))
+        self.submit = Button(self.white,
+                             text="Submit", command=self.check_answer)
+        self.submit.grid(row=3, column=0, columnspan=2, padx=180, pady=(20,20))
         self.label = Label(self.white, text="Answer: ")
-        self.label.grid(row=2, column=0)
-        self.answer = Entry(self.white)
-        self.answer.grid(row=2, column=1)
+        self.label.grid(row=2, column=0, padx=(100,0))
 
-        self.sidebar = LabelFrame(self.frame, background="blue", width=100)
-        self.sidebar.grid(column=3, row=0)
+        self.a = IntVar()
+        self.answer = Entry(self.white, textvariable=self.a)
+        self.answer.grid(row=2, column=1, padx=(0, 100))
+        self.question_generator()
+        self.grid()
 
-        self.nudge = Label(self.sidebar, text="Star is here to encourage you!")
-        self.nudge.grid(column=0, row=0)
+    def question_generator(self):
+        self.r = random.sample(range(0,100), 3)
+        a = f'{self.r[0]} + {self.r[1]} - {self.r[2]} = ?'
+        self.q = Label(self.white, background="#D0F6FC", text=a)
+        self.q.grid(row=1, column=0, columnspan=2, padx=200, pady=50) 
 
-    def check_answer(self):
+    def check_answer(self, event=None):
+        def clicked():
+            self.i = self.i + 1
         self.ans = int(self.answer.get())
+        self.question = self.r[0] + self.r[1] - self.r[2]
+        if self.ans == self.question:
+            SideBar('Correct!')
+        else: SideBar('Incorrect!')
+        clicked()
+        self.q_num = Label(self.white, 
+                           borderwidth=1, 
+                           background="yellow", 
+                           text=self.i)
+        self.q_num.grid(row=0, sticky=W)
         #Change input field into int
-        pass
+        if self.i <= 20:
+            self.next_q()
+        else: self.complete()
+
+    def next_q(self):
+        self.answer.delete(0, "end")
+        self.q.forget()
+        self.question_generator()
+
+    def complete(self):
+        self.popup = Button(self.white, text="STOP!", command=quit)
+        self.popup.grid()
+        #This is where the score calculations
+        #are stored into the student's data
+    
 
     def feedback(self):
         #For addition and subtraction 
         #For multiplication and division
         pass
 
-'''Frame 3 - Scoreboard page'''
-class scoreboard_win(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-
-        #Interface design
-        self.frame = LabelFrame(master, highlightbackground="orange")
-        self.frame.grid(column=0, row=0)
-
-        self.label = Label(self.frame, text="Your score is: ")
-        self.label.grid(row=0)
-        self.box = Canvas(self.frame, width=50, height=50)
-        self.box.grid(row=1, padx=50)
-        self.score_display = Label(self.box, text="score variable")
-        self.score_display.grid()
-        
-        self.b1 = Button(self.frame, text="Retry")
-        self.b1['command'] = self.retry
-        self.b1.grid(row=3, column=1)
-        self.b2 = Button(self.frame, text="New Player")
-        self.b2['command'] = self.new_player
-        self.b2.grid(row=3, column=2)
-
-    def calculate_score():
-        pass
-
-    def retry():
-        pass
-    
-    def new_player():
-        pass
-
-'''Functions for switching between frames - Ref: https://www.pythontutorial.net/tkinter/tkraise/'''
-#This is the main SwitchFrame function for the entire programme - need to see if works
-class SwitchFrame:
-    def __init__(self, container):
-        super().__init__(self, container)
-
-    # initialize frames
-        self.main_frames = {}
-        self.main_frames[0] = welcome_win(container) #static method in class to be inserted here later?
-        self.main_frames[1] = exercise_win(container)
-        self.main_frames[2] = scoreboard_win(container)
-
-        self.change_frame()
-
-    def change_frame(self):
-        frame = self.main_frames[self.selected_value.get()] #self.selected_value - button with value corresponding with window is clicked
-        frame.reset()
-        frame.tkraise()
-
-
-root = Tk()
-scoreboard_win(root)
-root.title("Ormiston Computing")
-root.geometry('1000x500')
-root.mainloop()
+if __name__ == "__main__":
+    root = SwitchFrame()
+    root.mainloop()
 
