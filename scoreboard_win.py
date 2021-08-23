286bbda61bfa3112b62dcea48d6860dc619cac5f
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-23 20:30:42 +1200

    Update for Switch frame

--- scoreboard_win.py
+++ scoreboard_win.py
@@ -1,33 +1,40 @@
 from tkinter import *
 
 '''Frame 3 - Scoreboard page'''
-class scoreboard_win(Frame):
-    def __init__(self, master=None):
+class ScoreboardWindow(Frame):
+    def __init__(self, master):
         Frame.__init__(self, master)
 
-        #Interface design
-        self.frame = LabelFrame(master, highlightbackground="orange")
-        self.frame.grid(column=0, row=0)
-
-        self.label = Label(self.frame, text="Your score is: ")
-        self.label.grid(row=0)
-        self.box = Canvas(self.frame, width=50, height=50)
-        self.box.grid(row=1, padx=50)
+        self.label = Label(self, text= f"Your score is: ")
+        self.label.grid(row=0, 
+                        column=0, 
+                        columnspan=3,
+                        padx=100,
+                        pady=(100,0))
+        self.box = LabelFrame(self, width=50, height=50, background="yellow")
+        self.box.grid(row=1,  
+                      column=0, 
+                      columnspan=3,
+                      padx=100,
+                      pady=(0,50))
         self.score_display = Label(self.box, text="score variable")
         self.score_display.grid()
         
-        self.b1 = Button(self.frame, text="Retry")
+        self.b1 = Button(self, text="Retry")
         self.b1['command'] = self.retry
-        self.b1.grid(row=3, column=1)
-        self.b2 = Button(self.frame, text="New Player")
+        self.b1.grid(row=3, column=1, padx=(100,0))
+        self.b2 = Button(self, text="New Player")
         self.b2['command'] = self.new_player
-        self.b2.grid(row=3, column=2)
+        self.b2.grid(row=3, column=2, padx=(100,0))
+
+        self.grid()
 
     def calculate_score():
         pass
 
     def retry():
-        pass
+        global selected_value
+        selected_value = 0
     
     def new_player():
         pass
