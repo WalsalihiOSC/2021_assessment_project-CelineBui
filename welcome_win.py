3e066b2a9be5e74946f9a6a8202dcf08f75acdca
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-24 10:27:37 +1200

    Layout changes + Entry box
    Placeholder text
    Submit button is deleted and instead substituted with the difficulty level
    Keep in mind that next page button "LET'S START!" only appear in main.py

--- welcome_win.py
+++ welcome_win.py
@@ -1,18 +1,38 @@
 from tkinter import *
 from student import Student
+from PIL import Image, ImageTk
 
 class WelcomeWindow(Frame):
     def __init__(self, master):
-        Frame.__init__(self, master)
+        Frame.__init__(self, master, background="#f0f0f0")
         #Interface - blank fields and buttons
 
+        #Entry boxes
+        #Code ref for working with textvariable: https://www.geeksforgeeks.org/python-tkinter-entry-widget/
         self.sn = StringVar() #Variable for name
         self.name_input = Entry(self, textvariable=self.sn, borderwidth=3)
-        self.name_input.grid(column=0, row=2, padx=10, pady=10)
-
+        #placeholder text in entry box
+        #Code ref: https://stackoverflow.com/questions/27820178/how-to-add-placeholder-to-an-entry-in-tkinter
+        self.name_input.insert(0, "Tell us your name here!")
+        #delete placeholder text when clicked
+        self.name_input.bind("<FocusIn>", 
+                                lambda args: self.name_input.delete('0', 'end')) 
+        self.name_input.grid(column=0, columnspan=2, row=3, padx=(150, 10), pady=10)
+        
         self.yr = IntVar()
-        self.year_level = Entry(self, textvariable=self.yr, borderwidth=3, width=10)
-        self.year_level.grid(column=1, row=2, padx=10, pady=10)
+        self.year_level = Entry(self, textvariable=self.yr, borderwidth=3, width=15)
+        self.year_level.insert(0, "Your year level?") 
+        self.year_level.bind("<FocusIn>", 
+                                lambda args: self.year_level.delete('0', 'end'))
+        self.year_level.grid(column=2, columnspan=2, row=3, padx=(10, 150), pady=10)
+
+        #Logo
+        self.im = Image.open('logo-ormmaths.png')
+        self.resize = self.im.resize((200, 85), Image.ANTIALIAS)
+        self.render = ImageTk.PhotoImage(self.resize)
+        self.img = Label(self, bg="#f2f2f2", image=self.render, height=85, width=170, padx=10)
+        self.img.image = self.render
+        self.img.grid(column=0, row=2, columnspan=4, sticky=S, ipadx=5, pady=(30,10))
 
         self.diff_value = IntVar()
         self.diff_value.set(1) #intialise choices i.e easy
@@ -30,25 +50,48 @@ class WelcomeWindow(Frame):
                         height=3,
                         variable = self.diff_value,
                         value = val,
-                        command = self.confirm).grid(column=2, sticky=W, padx=50, pady=(10,0))
+                        command = self.submit).grid(column=1,
+                                                     sticky=N,
+                                                     columnspan=1,
+                                                     padx=(100,0), 
+                                                     pady=(10,0))
         self.grid()
 
     def check_info(self):
         self.input_check = 1
-        if len(self.name_input.get()) == 0:
-            Label(self, text="Fill in your name!", fg="red").grid(column=0, row=1)
+        self.name = self.sn.get()
+        self.year = self.yr.get()
+        self.sn.set("")
+        self.yr.set("")
+        #Checking for blanks
+        if len(self.name) == 0:
+            Label(self, text="Fill in your name!", fg="red").grid(column=0, row=4)
             self.input_check = 0
+        if len(self.year) == 0:
+            Label(self, text="Fill in your year level!", fg="red").grid(column=0, row=4)
+            self.input_check = 0
+
+        #Invalids and boundaries - year level
+        try:
+            sy_number = int(self.year)
+        except ValueError:
+            Label(self.frame1, text="Enter whole numbers", fg="red", bg="#f2f2f2", font="Times 14").place(x=745, y=375)  
+            return False
+        if sy_number < 11 or sy_number > 13:
+            Label(self.frame1, text="Enter appropriate year level!", fg="red", bg="#f2f2f2", font="Times 14").place(x=745, y=375)
+            return False
+        return True
+
         if self.input_check == 1:
-            self.store_info()
-        
+            self.store_info() 
 
     def conf_message(self, a):
         Label(self, text=f"You've chosen {a} mode!").grid(column=2, 
-                                                          row=6, 
+                                                          row=5, 
                                                           ipadx=10, 
                                                           pady=(20,0))
 
-    def confirm(self):
+    def submit(self):
         '''if len(self.student) == 3:
             self.student[2] = None'''
         self.diff_lvl = StringVar()
@@ -75,5 +118,6 @@ class WelcomeWindow(Frame):
         self.difficulty = None
 
 root = Tk()
+root.geometry('800x400')
 WelcomeWindow(root)
 root.mainloop()
