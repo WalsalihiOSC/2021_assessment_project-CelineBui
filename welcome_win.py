4816cbb1af3c5d963df65f4ebe6e5841d23029fd
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-23 20:31:27 +1200

    Update for Switch Frame

--- welcome_win.py
+++ welcome_win.py
@@ -2,64 +2,61 @@ from tkinter import *
 from student import Student
 
 class WelcomeWindow(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self)
-        self.student = []
+    def __init__(self, master):
+        Frame.__init__(self, master)
         #Interface - blank fields and buttons
 
         self.sn = StringVar() #Variable for name
         self.name_input = Entry(self, textvariable=self.sn, borderwidth=3)
-        self.name_input.grid(column=0, row=0, padx=10, pady=10)
+        self.name_input.grid(column=0, row=2, padx=10, pady=10)
 
         self.yr = IntVar()
         self.year_level = Entry(self, textvariable=self.yr, borderwidth=3, width=10)
-        self.year_level.grid(column=1, row=0, padx=10, pady=10)
+        self.year_level.grid(column=1, row=2, padx=10, pady=10)
 
-        self.conf = Button(self, text="LET'S START!")
-        self.conf.grid(column=2, row=4, pady=15, ipady=10, ipadx=10, sticky=E)
-        self.conf.configure(command=self.check_info)
-
-        self.diff = IntVar()
-        self.diff.set(1) #intialise choices i.e easy
+        self.diff_value = IntVar()
+        self.diff_value.set(1) #intialise choices i.e easy
 
         choices = [ ("Easy", 101),
                     ("Kinda easy", 102),
                     ("Not so easy", 103)]
-
-        self.sideframe = LabelFrame(self, height=10).grid(column=2, row=0)
+        #buttons for difficulty levels
+        #Code ref: https://www.python-course.eu/tkinter_radiobuttons.php 
         for choice, val in choices:
-            Radiobutton(self.sideframe,
-                        text = choice,
+            self.choices = Radiobutton(self,
+                                       text = choice,
                         indicatoron = 0,
                         width=10,
                         height=3,
-                        variable = self.diff,
+                        variable = self.diff_value,
                         command = self.confirm,
                         value = val).grid(column=2, sticky=W, padx=50, pady=(10,0))
-
         self.grid()
 
     def check_info(self):
-        if self.sn == " ":
-            Label(self, text="Fill in your name!", fg="red").grid(column=0, row=2)
+        self.input_check = 1
+        if len(self.name_input.get()) == 0:
+            Label(self, text="Fill in your name!", fg="red").grid(column=0, row=1)
+            self.input_check = 0
+        self.confirm()
         
     def store_info(self):
         #storing info when user clicks any of the difficulty level buttons to move forward
         self.student.append(self.name_input.get(), self.diff())
-        self.confirm()
 
     def confirm(self):
         '''if len(self.student) == 3:
             self.student[2] = None'''
-        Label(self.sideframe, text=f"You've chosen {self.diff} mode!")
-
+        if self.diff_value == 101:
+            self.diff = "Easy"
+        if self.diff_value == 102:
+            self.diff = "Kinda easy"
+        if self.diff_value == 103:
+            self.diff = "Not so easy"
+            return self.diff
+        Label(self, text=f"You've chosen {self.diff} mode!").grid(column=2, row=5)
+        self.store_info()
 
     def reset(self):
         self.name_input.delete(0, 'end')
         self.difficulty = None
-
-root = Tk()
-root.title("Ormiston Computing")
-root.geometry("800x320")
-WelcomeWindow(root)
-root.mainloop()
