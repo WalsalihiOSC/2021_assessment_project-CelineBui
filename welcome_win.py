c036875f0be75f6188b573dda64df740d4bb19f1
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-23 21:44:51 +1200

    Working Confirmation for Diff level

--- welcome_win.py
+++ welcome_win.py
@@ -23,14 +23,14 @@ class WelcomeWindow(Frame):
         #buttons for difficulty levels
         #Code ref: https://www.python-course.eu/tkinter_radiobuttons.php 
         for choice, val in choices:
-            self.choices = Radiobutton(self,
-                                       text = choice,
-                        indicatoron = 0,
+            Radiobutton(self,
+                        text = choice,
+                        indicatoron=0,
                         width=10,
                         height=3,
                         variable = self.diff_value,
-                        command = self.confirm,
-                        value = val).grid(column=2, sticky=W, padx=50, pady=(10,0))
+                        value = val,
+                        command = self.confirm).grid(column=2, sticky=W, padx=50, pady=(10,0))
         self.grid()
 
     def check_info(self):
@@ -38,25 +38,42 @@ class WelcomeWindow(Frame):
         if len(self.name_input.get()) == 0:
             Label(self, text="Fill in your name!", fg="red").grid(column=0, row=1)
             self.input_check = 0
-        self.confirm()
+        if self.input_check == 1:
+            self.store_info()
         
-    def store_info(self):
-        #storing info when user clicks any of the difficulty level buttons to move forward
-        self.student.append(self.name_input.get(), self.diff())
+
+    def conf_message(self, a):
+        Label(self, text=f"You've chosen {a} mode!").grid(column=2, 
+                                                          row=6, 
+                                                          ipadx=10, 
+                                                          pady=(20,0))
 
     def confirm(self):
         '''if len(self.student) == 3:
             self.student[2] = None'''
-        if self.diff_value == 101:
-            self.diff = "Easy"
-        if self.diff_value == 102:
-            self.diff = "Kinda easy"
-        if self.diff_value == 103:
-            self.diff = "Not so easy"
-            return self.diff
-        Label(self, text=f"You've chosen {self.diff} mode!").grid(column=2, row=5)
-        self.store_info()
+        self.diff_lvl = StringVar()
+        if self.diff_value.get() == 101:
+            self.conf_message("Easy")
+            self.diff_lvl = "Easy"
+            return self.diff_lvl
+        elif self.diff_value.get() == 102:
+            self.conf_message("Kinda easy")
+            self.diff_lvl = "Kinda easy"
+            return self.diff_lvl
+        elif self.diff_value.get() == 103:
+            self.conf_message("Not so easy")
+            self.diff_lvl = "Not so easy"
+            return self.diff_lvl
+        self.check_info()
+
+    def store_info(self):
+        #storing info when user clicks any of the difficulty level buttons to move forward
+        print(self.sn, self.yr, self.diff_value.get(), self.diff_lvl)
 
     def reset(self):
         self.name_input.delete(0, 'end')
         self.difficulty = None
+
+root = Tk()
+WelcomeWindow(root)
+root.mainloop()
