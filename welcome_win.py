f8e3e0d90867df1ccc0922bbd62d3413ebbfe3ce
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-08-25 14:53:06 +1200

    Update error checking + work with Frame Switch

--- welcome_win.py
+++ welcome_win.py
@@ -18,23 +18,20 @@ class WelcomeWindow(Frame):
         #Code ref for working with textvariable: https://www.geeksforgeeks.org/python-tkinter-entry-widget/
         self.sn = StringVar() #Variable for name
         self.name_input = Entry(self, textvariable=self.sn, borderwidth=3)
-        #placeholder text in entry box
-        #Code ref: https://stackoverflow.com/questions/27820178/how-to-add-placeholder-to-an-entry-in-tkinter
-        self.name_input.insert(0, "Click here to tell us your name!")
+        #placeholder text in entry box. Code ref: https://stackoverflow.com/questions/27820178/how-to-add-placeholder-to-an-entry-in-tkinter
+        self.name_input.insert(0, "Tell us your name!")
         #delete placeholder text when clicked
         self.name_input.bind("<FocusIn>", 
                                 lambda args: self.name_input.delete('0', 'end')) 
         self.name_input.grid(column=0, columnspan=2, row=3, padx=(200, 10), pady=10)
         
-        self.yr = StringVar()
+        self.yr = IntVar()
         self.year_level = Entry(self, textvariable=self.yr, borderwidth=3, width=15)
         self.year_level.insert(0, "Your year level?") 
         self.year_level.bind("<FocusIn>", 
                                 lambda args: self.year_level.delete('0', 'end'))
         self.year_level.grid(column=2, columnspan=2, row=3, padx=(10, 250), pady=10)
         
-        self.sn.set("")
-        self.yr.set("")
 
         self.diff_value = IntVar()
         self.diff_lvl = StringVar()
@@ -61,40 +58,44 @@ class WelcomeWindow(Frame):
         self.grid()
 
     def store_info(self):
-        if len(Student.attr_list) != 0: #Clear list if user clicks differnt diff level
-            Student.attr_list.append((self.name, self.year, self.diff_lvl))
-            print(self.name, self.year, self.diff_lvl)
+        if len(Student.attr_list) == 0: #Clear list if user clicks differnt diff level
+            Student.attr_list.extend([self.sn.get(), self.yr.get(), self.diff_lvl])
             print(Student.attr_list)
             print(len(Student.attr_list))
             return True
-        else: 
+        elif len(Student.attr_list) > 3: 
+            #if students choose a different diff lvl
+            #i.e clicking 'easy' and then click 'not so easy'
+            #Student.attr_list will be reset
             Student.attr_list = []
             return False
         
     
-    def check_info(self):
+    def check_info(self, event=None):
         self.input_check = 1
         #Drawing value from entry boxes
         #Checking for blanks
-        if len(self.name) == 0:
+        if len(self.sn.get()) == 0:
             Label(self, text="Fill in your name!", fg="red").grid(column=0, row=4)
             self.input_check = 0
-        if len(self.year) == 0:
+        if self.yr.get() == "":
             Label(self, text="Fill in your year level!", fg="red").grid(column=3, row=4)
             self.input_check = 0
-
         #Invalids and boundaries - year level
         try:
-            sy_number = int(self.year)
+            sy_number = int(self.yr.get())
+            return sy_number, True
         except ValueError:
             Label(self, text="You must be in Year 1 to Year 6.", fg="red").grid(column=3, row=4)  
             self.input_check = 0
-        if sy_number < 1 or sy_number > 6:
-            Label(self, text="You must be in Year 1 to Year 6.", fg="red", bg="#f0f0f0", font="Times 14").grid(column=3, row=4)
-            self.input_check = 0
+
+        if self.yr.get() < 1 or self.yr.get() > 6:
+            Label(self, text="You must be in Year 1 to Year 6.", fg="red", bg="#f0f0f0").grid(column=3, row=4)
+            self.input_check = 0 
 
         if self.input_check == 1:
             self.store_info()
+           
 
     def conf_message(self, a):
         Label(self, text=f"You've chosen\n{a} mode!").grid(column=2, columnspan=2,
@@ -103,8 +104,7 @@ class WelcomeWindow(Frame):
                                                           pady=(20,0))
 
     def submit(self):
-        self.name = self.sn.get()
-        self.year = self.yr.get()
+        self.check_info()
         '''if len(self.student) == 3:
             self.student[2] = None'''
         if self.diff_value.get() == 101:
@@ -116,14 +116,8 @@ class WelcomeWindow(Frame):
         elif self.diff_value.get() == 103:
             self.conf_message("Not so easy")
             self.diff_lvl = "Not so easy"
-        self.check_info()
         return self.diff_lvl
 
     def reset(self):
         self.name_input.delete(0, 'end')
         self.difficulty = None
-
-'''root = Tk()
-root.geometry('800x400')
-WelcomeWindow(root)
-root.mainloop()'''
