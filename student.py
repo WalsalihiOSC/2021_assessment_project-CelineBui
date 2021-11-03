80ba0aa1ff051e00376b3216899360556e3810da
CelineBui <79140450+CelineBui@users.noreply.github.com>
2021-11-03 23:20:06 +1300

    Complete version - DATA ENCAP

--- student.py
+++ student.py
@@ -14,14 +14,43 @@ class Student:
     def class_obj(self):
         #Data encapsulation as soon as when final frame initiates - all attributes contain info
         '''     All data checking must have occured before this!    '''
+        if len(Student.attr_list) == 4:
+            self.student_name = Student.attr_list[0]
+            self.year_level = Student.attr_list[1]
+            self.diff_lvl = Student.attr_list[2]
+            self.score = Student.attr_list[3]
         self.student = Student(self.student_name, self.year_level, self.diff_lvl, self.score)
         #Print self.student object as a string. Code ref: https://www.educative.io/edpresso/what-is-the-str-method-in-python
         print(self.student.__str__())
 
+    def retry(self):
+        #New score will become the score in the object's attribute scr
+        #However old score will not be erased from file
+        #New score is added with title "Second attempt"
+        #Code ref: https://www.programiz.com/python-programming/methods/list/pop 
+        Student.write_file(self.student)
+        self.retry_count = Student.retry_times
+        self.retry_count = self.retry_count + 1
+        self.score_2 = Student.attr_list.pop(3) #replaces student's old score with new one in instance
+        print(self.score_2)
+        print(len(Student.attr_list))
+        if len(Student.attr_list) == 4:
+            with open("students.txt", 'a', encoding = 'utf-8') as f: #file always closes even when operations unexpectedly fail
+                f.write(f"Attempt {self.retry_count}: {self.score}")
+    
+    def new_player(self):
+        #Only write to file if click new player
+        Student.write_file(self.student)
+        with open("students.txt", 'a', encoding = 'utf-8') as f: #file always closes even when operations unexpectedly fail
+            f.write(f"===========\n\n")
+        self.student = None
+    
     def write_file(self):
         #No divider in the write text file to append additional info of later attempts
         with open("students.txt", 'a', encoding = 'utf-8') as f: #file always closes even when operations unexpectedly fail
             f.write(f"===========\nStudent's name: {self.student_name}\nDifficulty level: {self.diff_lvl}\nStudent's score: {self.score}\n")
+
+
 
 class CheckingInput:
     check = [] #list used for communicating between instance and interface
@@ -34,19 +63,20 @@ class CheckingInput:
         CheckingInput.invalid = 0
         #Assuming info has been stoed into checking list
         if len(CheckingInput.check) == 3: #3 attr of name, year and diff lvl
-            print(CheckingInput.check)
             self.name = CheckingInput.check[0]
-            self.year = CheckingInput.check[1]
             self.diff = CheckingInput.check[2]
-            print(type(self.year))
             #Invalid checking
             try:
-                self.year = int(CheckingInput.check[1])
+                year = int(CheckingInput.check[1])
+                return year
             except ValueError: #player used text instead of number for year lvl
                 CheckingInput.invalid = 2
             except TclError:
                 CheckingInput.invalid = 2
                 ''' 2 = text instead of num '''
+            if CheckingInput.invalid != 2:
+                self.year = int(CheckingInput.check[1])
+                print(type(self.year))
             
             #Boundaries for year lvl
             if CheckingInput.invalid != 2:
@@ -73,23 +103,3 @@ class CheckingInput:
 
 
 
-    '''def retry(self):
-        #New score will become the score in the object's attribute scr
-        #However old score will not be erased from file
-        #New score is added with title "Second attempt"
-        #Code ref: https://www.programiz.com/python-programming/methods/list/pop 
-        Student.write_file(self.student)
-        self.retry_count = Student.retry_times
-        self.retry_count = self.retry_count + 1
-        self.score_2 = Student.attr_list.pop(3) #replaces student's old score with new one in instance
-        print(self.score_2)
-        print(len(Student.attr_list))
-        if len(Student.attr_list) == 4:
-            with open("students.txt", 'a', encoding = 'utf-8') as f: #file always closes even when operations unexpectedly fail
-                f.write(f"Attempt {self.retry_count}: {self.score}")
-    
-    def new_player(self):
-        #Only write to file if click new player
-        Student.write_file(self.student)
-        with open("students.txt", 'a', encoding = 'utf-8') as f: #file always closes even when operations unexpectedly fail
-            f.write(f"===========\n\n")'''
